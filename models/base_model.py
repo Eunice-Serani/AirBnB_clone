5c7e92cb3a403e304e03b9e303bf83042f959f5e
Eunice-Serani <eaamable91@gmail.com>
2024-01-14 23:54:17 +0000

    Update model

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,21 @@
-#!/usr/bin/python3
+import uuid
 from datetime import datetime
-from uuid import uuid4
 
-from models import storage
-
-
-class BaseModel():
-    """ class BaseModel that defines all common
-    attributes/methods for other classes"""
-
-    def __init__(self, *args, **kwargs):
-        """ entry point """
-        if kwargs is not None and kwargs.get('id') is not None:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        self.__setattr__(key, datetime.fromisoformat(value))
-                    else:
-                        self.__setattr__(key, value)
-
-        else:
-            self.id = str(uuid4())
-            # current datetime when an instance is created
-            self.created_at = datetime.now()
-            # when instance is created and updated every time object is changed
-            self.updated_at = self.created_at
-            storage.new(self)
-
-    def save(self):
-        """updates the public instance attribute update_at
-        with the current datetime"""
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-        ky = {key: value for key, value in self.__dict__.items()}
-        ky['__class__'] = self.__class__.__name__
-        ky['created_at'] = self.created_at.isoformat()
-        ky['updated_at'] = self.updated_at.isoformat()
-        return ky
 
     def __str__(self):
-        """string representation"""
-        cl_name = self.__class__.__name__
-        ky = {key: value for key, value in self.__dict__.items()}
-        return("[{}] ({}) {}".format(cl_name, self.id, ky))
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
