1ba59bfeff1bdf08e4210b0511b0caf6280e7a0d
Eunice-Serani <eaamable91@gmail.com>
2024-01-15 01:52:04 +0000

    More classes

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ User class """
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes User instance """
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+
+    def __str__(self):
+        """ Returns the string representation of User """
+        return "[{}] ({}) {} {}".format(self.__class__.__name__, self.id, self.first_name, self.last_name)
